178ffe58c050f26236cd905b0b44c62998e9fa7e
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-25 01:10:26 +0600

    comparison == and ===

---
+++ double-triple.js
@@ -0,0 +1,26 @@
+
+// no : 1
+/* const first = 4; // apply 1, 2, 0, '0', '2'
+const second = 4;
+if(first === second){
+    console.log('condition  is true');
+}
+else{
+    console.log('condition is false');
+} */
+
+// no : 2
+/* 
+const a = { name: 'mofu'};
+const b = { name: 'mofu'}; */
+const a = [];
+const b = [];
+
+if(a == b){
+    console.log('Both are same');
+}
+else{
+    console.log('They are not same');
+}
+
+
